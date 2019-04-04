3a11cb79d703954de0aee9a65f7657ceb29e3063
Javier <javieroviedo2000@gmail.com>
2019-04-04 00:15:32 -0600

    deleted unnecesary files

--- drive-payment2/drive/PaymentDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace drive
-{
-    public partial class PaymentDetail : Gtk.Window
-    {
-        public PaymentDetail() :
-                base(Gtk.WindowType.Toplevel) => this.Build();
-    }
-}
